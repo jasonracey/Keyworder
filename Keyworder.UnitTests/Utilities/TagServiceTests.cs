b1b35d971afb0a3cbf54c3170201593e9caca937
Jason Racey <jasonracey@gmail.com>
2021-11-30 19:30:52 -0800

    Add tag service and tests

---
+++ Keyworder.UnitTests/Utilities/TagServiceTests.cs
@@ -0,0 +1,60 @@
+﻿using Keyworder.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Keyworder.UnitTests.Utilities
+{
+    [TestClass]
+    public class TagServiceTests
+    {
+        [TestMethod]
+        public void WhenCollectionIsNull_ThrowsExpectedException()
+        {
+            // act/assert
+            Assert.ThrowsException<ArgumentNullException>(() => TagService.ToFlickrTagsString(null));
+        }
+
+        [TestMethod]
+        public void WhenCollectionIsEmpty_ReturnsEmptyString()
+        {
+            // arrange
+            var values = new List<string?>();
+
+            // act
+            var result = TagService.ToFlickrTagsString(values);
+
+            // assert
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [DataTestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow(" ")]
+        public void WhenNullOrWhiteSpace_ValueIsExcluded(string value)
+        {
+            // arrange
+            var values = new List<string> { value };
+
+            // act
+            var result = TagService.ToFlickrTagsString(values);
+
+            // assert
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [TestMethod]
+        public void ValuesAre_Quoted_CommaDelimited_SortedCaseInsensitive()
+        {
+            // arrange
+            var values = new List<string> { "b", "c", "B", "a" };
+
+            // act
+            var result = TagService.ToFlickrTagsString(values);
+
+            // assert
+            Assert.AreEqual("\"a\",\"b\",\"B\",\"c\"", result);
+        }
+    }
+}
---
+++ Keyworder/Utilities/TagService.cs
@@ -0,0 +1,17 @@
+﻿namespace Keyworder.Utilities;
+
+public static class TagService
+{
+    public static string ToFlickrTagsString(IEnumerable<string?> values)
+    {
+        if (values == null)
+            throw new ArgumentNullException(nameof(values));
+
+        var quotedOrderedValues = values
+            .Where(value => !string.IsNullOrWhiteSpace(value))
+            .OrderBy(value => value, StringComparer.OrdinalIgnoreCase)
+            .Select(value => $"\"{value}\"");
+
+        return string.Join(",", quotedOrderedValues);
+    }
+}
