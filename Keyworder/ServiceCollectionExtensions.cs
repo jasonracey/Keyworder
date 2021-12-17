fdc0c6a58768b7c280df08844563210221da7943
Jason Racey <jasonracey@gmail.com>
2021-12-16 17:07:57 -0800

    Add notification to edit keyword

--- Keyworder/ServiceCollectionExtensions.cs
+++ Keyworder/ServiceCollectionExtensions.cs
@@ -1,13 +1,32 @@
 ﻿using Keyworder.Data;
+using Radzen;
 
 namespace Keyworder;
 
 public static class ServiceCollectionExtensions
 {
-    public static void AddKeyworderServices(this IServiceCollection services, ConfigurationManager configuration)
+    public static void AddKeyworderServices(
+        this IServiceCollection services, 
+        ConfigurationManager configuration)
+    {
+        services
+            .AddKeywordService(configuration)
+            .AddNotificationService();
+    }
+
+    private static IServiceCollection AddKeywordService(
+        this IServiceCollection services, 
+        ConfigurationManager configuration)
     {
         var keywordsJsonPath = configuration["KeywordsJsonPath"];
         var keywordService = new KeywordService(keywordsJsonPath);
-        services.AddSingleton(keywordService);
+        return services.AddSingleton(keywordService);
+    }
+
+    private static IServiceCollection AddNotificationService(
+        this IServiceCollection services)
+    {
+        var notificationService = new NotificationService();
+        return services.AddSingleton(notificationService);
     }
 }
