e01bb7ca7a535b43fbe5dfbcc8c200a4d6cec9d1
Jason Racey <jasonracey@gmail.com>
2021-12-24 09:32:16 -0800

    Additional keyword service tests

--- Keyworder.UnitTests/Data/KeywordServiceTests.cs
+++ Keyworder.UnitTests/Data/KeywordServiceTests.cs
@@ -52,6 +52,7 @@ public class KeywordServiceTests : TestBase
         // assert
         result.Should().Be(ResultType.Created);
         await AssertCategoryExists(categoryNameClean);
+        await AssertCategoryDoesNotExist(categoryName);
     }
 
     [TestMethod]
@@ -67,6 +68,7 @@ public class KeywordServiceTests : TestBase
         // assert
         result.Should().Be(ResultType.Created);
         await AssertCategoryExists(categoryNameClean);
+        await AssertCategoryDoesNotExist(categoryName);
     }
 
     [TestMethod]
@@ -74,9 +76,9 @@ public class KeywordServiceTests : TestBase
     {
         // arrange
         var categoryName = Guid.NewGuid().ToString();
+        var firstResult = await keywordService.CreateCategoryAsync(categoryName);
 
         // act
-        var firstResult = await keywordService.CreateCategoryAsync(categoryName);
         var secondResult = await keywordService.CreateCategoryAsync(categoryName);
 
         // assert
@@ -118,8 +120,8 @@ public class KeywordServiceTests : TestBase
     {
         // arrange
         var categoryNameClean = Guid.NewGuid().ToString();
-        var keywordNameClean = Guid.NewGuid().ToString();
         var categoryName = $" {categoryNameClean}  ";
+        var keywordNameClean = Guid.NewGuid().ToString();
         var keywordName = $" {keywordNameClean}  ";
         var categoryResult = await keywordService.CreateCategoryAsync(categoryName);
 
@@ -130,7 +132,9 @@ public class KeywordServiceTests : TestBase
         categoryResult.Should().Be(ResultType.Created);
         keywordResult.Should().Be(ResultType.Created);
         await AssertCategoryExists(categoryNameClean);
+        await AssertCategoryDoesNotExist(categoryName);
         await AssertKeywordExists(categoryNameClean, keywordNameClean);
+        await AssertKeywordDoesNotExist(categoryNameClean, keywordName);
     }
 
     [TestMethod]
@@ -150,7 +154,9 @@ public class KeywordServiceTests : TestBase
         categoryResult.Should().Be(ResultType.Created);
         keywordResult.Should().Be(ResultType.Created);
         await AssertCategoryExists(categoryNameClean);
+        await AssertCategoryDoesNotExist(categoryName);
         await AssertKeywordExists(categoryNameClean, keywordNameClean);
+        await AssertKeywordDoesNotExist(categoryNameClean, keywordName);
     }
 
     [TestMethod]
@@ -226,6 +232,7 @@ public class KeywordServiceTests : TestBase
         // assert
         createResult.Should().Be(ResultType.Created);
         deleteResult.Should().Be(ResultType.Deleted);
+        await AssertCategoryDoesNotExist(categoryName);
         await AssertCategoryDoesNotExist(categoryNameClean);
     }
 
@@ -293,6 +300,8 @@ public class KeywordServiceTests : TestBase
         createCategoryResult.Should().Be(ResultType.Created);
         createKeywordResult.Should().Be(ResultType.Created);
         deleteResult.Should().Be(ResultType.Deleted);
+        await AssertCategoryExists(categoryNameClean);
+        await AssertCategoryDoesNotExist(categoryName);
         await AssertKeywordDoesNotExist(categoryNameClean, keywordName);
         await AssertKeywordDoesNotExist(categoryNameClean, keywordNameClean);
     }
@@ -315,13 +324,254 @@ public class KeywordServiceTests : TestBase
         createCategoryResult.Should().Be(ResultType.Created);
         createKeywordResult.Should().Be(ResultType.Created);
         deleteResult.Should().Be(ResultType.Deleted);
+        await AssertCategoryExists(categoryNameClean);
+        await AssertCategoryDoesNotExist(categoryName);
         await AssertKeywordDoesNotExist(categoryNameClean, keywordName);
         await AssertKeywordDoesNotExist(categoryNameClean, keywordNameClean);
     }
 
-    // edit category
+    [DataTestMethod]
+    [DataRow(null)]
+    [DataRow("")]
+    [DataRow(" ")]
+    public async Task EditCategory_ValidatesOldCategoryArg(string oldCategoryName)
+    {
+        // arrange
+        var task = async () => { await keywordService.EditCategoryAsync(oldCategoryName, "newCategory"); };
 
-    // edit keyword
+        // act/assert
+        await task.Should().ThrowAsync<ArgumentNullException>()
+            .WithParameterName("oldCategoryName");
+    }
+
+    [DataTestMethod]
+    [DataRow(null)]
+    [DataRow("")]
+    [DataRow(" ")]
+    public async Task EditCategory_ValidatesNewCategoryArg(string newCategoryName)
+    {
+        // arrange
+        var task = async () => { await keywordService.EditCategoryAsync("oldCategory", newCategoryName); };
+
+        // act/assert
+        await task.Should().ThrowAsync<ArgumentNullException>()
+            .WithParameterName("newCategoryName");
+    }
+
+    [TestMethod]
+    public async Task EditCategory_TrimsInput()
+    {
+        // arrange
+        var oldCategoryNameClean = Guid.NewGuid().ToString();
+        var oldCategoryName = $" {oldCategoryNameClean}  ";
+        var newCategoryNameClean = Guid.NewGuid().ToString();
+        var newCategoryName = $" {newCategoryNameClean}  ";
+        var createResult = await keywordService.CreateCategoryAsync(oldCategoryName);
+
+        // act
+        var editResult = await keywordService.EditCategoryAsync(oldCategoryName, newCategoryName);
+
+        // assert
+        createResult.Should().Be(ResultType.Created);
+        editResult.Should().Be(ResultType.Edited);
+        await AssertCategoryDoesNotExist(oldCategoryNameClean);
+        await AssertCategoryDoesNotExist(oldCategoryName);
+        await AssertCategoryDoesNotExist(newCategoryName);
+        await AssertCategoryExists(newCategoryNameClean);
+    }
+
+    [TestMethod]
+    public async Task EditCategory_HtmlEncodesInput()
+    {
+        // arrange
+        var oldCategoryName = "<script>alert('hi');</script>";
+        var oldCategoryNameClean = HttpUtility.HtmlEncode(oldCategoryName);
+        var newCategoryName = "<script>alert('world');</script>";
+        var newCategoryNameClean = HttpUtility.HtmlEncode(newCategoryName);
+        var createResult = await keywordService.CreateCategoryAsync(oldCategoryName);
+
+        // act
+        var editResult = await keywordService.EditCategoryAsync(oldCategoryName, newCategoryName);
+
+        // assert
+        createResult.Should().Be(ResultType.Created);
+        editResult.Should().Be(ResultType.Edited);
+        await AssertCategoryDoesNotExist(oldCategoryNameClean);
+        await AssertCategoryDoesNotExist(oldCategoryName);
+        await AssertCategoryDoesNotExist(newCategoryName);
+        await AssertCategoryExists(newCategoryNameClean);
+    }
+
+    [TestMethod]
+    public async Task EditCategory_PreventsDuplicateCategory()
+    {
+        // arrange
+        var oldCategoryName = Guid.NewGuid().ToString();
+        var newCategoryName = Guid.NewGuid().ToString();
+        var oldCreateResult = await keywordService.CreateCategoryAsync(oldCategoryName);
+        var newCreateResult = await keywordService.CreateCategoryAsync(newCategoryName);
+
+        // act
+        var editResult = await keywordService.EditCategoryAsync(oldCategoryName, newCategoryName);
+
+        // assert
+        oldCreateResult.Should().Be(ResultType.Created);
+        newCreateResult.Should().Be(ResultType.Created);
+        editResult.Should().Be(ResultType.Duplicate);
+        await AssertCategoryExists(oldCategoryName);
+        await AssertCategoryExists(newCategoryName);
+    }
+
+    [DataTestMethod]
+    [DataRow(null)]
+    [DataRow("")]
+    [DataRow(" ")]
+    public async Task EditKeyword_ValidatesCategoryArg(string categoryName)
+    {
+        // arrange
+        var task = async () => { await keywordService.EditKeywordAsync(categoryName, "oldKeyword", "newKeyword"); };
+
+        // act/assert
+        await task.Should().ThrowAsync<ArgumentNullException>()
+            .WithParameterName("categoryName");
+    }
+
+    [DataTestMethod]
+    [DataRow(null)]
+    [DataRow("")]
+    [DataRow(" ")]
+    public async Task EditKeyword_ValidatesOldKeywordArg(string oldKeywordName)
+    {
+        // arrange
+        var task = async () => { await keywordService.EditKeywordAsync("category", oldKeywordName, "newKeyword"); };
+
+        // act/assert
+        await task.Should().ThrowAsync<ArgumentNullException>()
+            .WithParameterName("oldKeywordName");
+    }
+
+    [DataTestMethod]
+    [DataRow(null)]
+    [DataRow("")]
+    [DataRow(" ")]
+    public async Task EditCategory_ValidatesNewKeywordArg(string newKeywordName)
+    {
+        // arrange
+        var task = async () => { await keywordService.EditKeywordAsync("category", "oldKeyword", newKeywordName); };
+
+        // act/assert
+        await task.Should().ThrowAsync<ArgumentNullException>()
+            .WithParameterName("newKeywordName");
+    }
+
+    [TestMethod]
+    public async Task EditKeyword_TrimsInput()
+    {
+        // arrange
+        var categoryNameClean = Guid.NewGuid().ToString();
+        var categoryName = $" {categoryNameClean}  ";
+        var oldKeywordNameClean = Guid.NewGuid().ToString();
+        var oldKeywordName = $" {oldKeywordNameClean}  ";
+        var newKeywordNameClean = Guid.NewGuid().ToString();
+        var newKeywordName = $" {newKeywordNameClean}  ";
+        var createCategoryResult = await keywordService.CreateCategoryAsync(categoryName);
+        var createOldKeywordResult = await keywordService.CreateKeywordAsync(categoryName, oldKeywordName);
+
+        // act
+        var editResult = await keywordService.EditKeywordAsync(categoryName, oldKeywordName, newKeywordName);
+
+        // assert
+        createCategoryResult.Should().Be(ResultType.Created);
+        createOldKeywordResult.Should().Be(ResultType.Created);
+        editResult.Should().Be(ResultType.Edited);
+        await AssertCategoryDoesNotExist(categoryName);
+        await AssertCategoryExists(categoryNameClean);
+        await AssertKeywordDoesNotExist(categoryNameClean, oldKeywordName);
+        await AssertKeywordDoesNotExist(categoryNameClean, oldKeywordNameClean);
+        await AssertKeywordDoesNotExist(categoryNameClean, newKeywordName);
+        await AssertKeywordExists(categoryNameClean, newKeywordNameClean);
+    }
+
+    [TestMethod]
+    public async Task EditKeyword_HtmlEncodesInput()
+    {
+        // arrange
+        var categoryName = "<script>alert('foo');</script>";
+        var categoryNameClean = HttpUtility.HtmlEncode(categoryName);
+        var oldKeywordName = "<script>alert('bar');</script>";
+        var oldKeywordNameClean = HttpUtility.HtmlEncode(oldKeywordName);
+        var newKeywordName = "<script>alert('baz');</script>";
+        var newKeywordNameClean = HttpUtility.HtmlEncode(newKeywordName);
+        var createCategoryResult = await keywordService.CreateCategoryAsync(categoryName);
+        var createOldKeywordResult = await keywordService.CreateKeywordAsync(categoryName, oldKeywordName);
+
+        // act
+        var editResult = await keywordService.EditKeywordAsync(categoryName, oldKeywordName, newKeywordName);
+
+        // assert
+        createCategoryResult.Should().Be(ResultType.Created);
+        createOldKeywordResult.Should().Be(ResultType.Created);
+        editResult.Should().Be(ResultType.Edited);
+        await AssertCategoryDoesNotExist(categoryName);
+        await AssertCategoryExists(categoryNameClean);
+        await AssertKeywordDoesNotExist(categoryNameClean, oldKeywordName);
+        await AssertKeywordDoesNotExist(categoryNameClean, oldKeywordNameClean);
+        await AssertKeywordDoesNotExist(categoryNameClean, newKeywordName);
+        await AssertKeywordExists(categoryNameClean, newKeywordNameClean);
+    }
+
+    [TestMethod]
+    public async Task EditKeyword_AllowsDuplicateKeywordInDifferentCategory()
+    {
+        // arrange
+        var firstCategory = Guid.NewGuid().ToString();
+        var secondCategory = Guid.NewGuid().ToString();
+        var firstKeyword = Guid.NewGuid().ToString();
+        var secondKeyword = Guid.NewGuid().ToString();
+        var firstCategoryResult = await keywordService.CreateCategoryAsync(firstCategory);
+        var secondCategoryResult = await keywordService.CreateCategoryAsync(secondCategory);
+        var firstKeywordResult = await keywordService.CreateKeywordAsync(firstCategory, firstKeyword);
+        var secondKeywordResult = await keywordService.CreateKeywordAsync(secondCategory, secondKeyword);
+
+        // act
+        var editResult = await keywordService.EditKeywordAsync(firstCategory, firstKeyword, secondKeyword);
+
+        // assert
+        firstCategoryResult.Should().Be(ResultType.Created);
+        secondCategoryResult.Should().Be(ResultType.Created);
+        firstKeywordResult.Should().Be(ResultType.Created);
+        secondKeywordResult.Should().Be(ResultType.Created);
+        editResult.Should().Be(ResultType.Edited);
+        await AssertCategoryExists(firstCategory);
+        await AssertCategoryExists(secondCategory);
+        await AssertKeywordDoesNotExist(firstCategory, firstKeyword);
+        await AssertKeywordExists(firstCategory, secondKeyword);
+        await AssertKeywordExists(secondCategory, secondKeyword);
+    }
+
+    [TestMethod]
+    public async Task EditKeyword_PreventsDuplicateKeywordInSameCategory()
+    {
+        // arrange
+        var category = Guid.NewGuid().ToString();
+        var firstKeyword = Guid.NewGuid().ToString();
+        var secondKeyword = Guid.NewGuid().ToString();
+        var categoryResult = await keywordService.CreateCategoryAsync(category);
+        var firstKeywordResult = await keywordService.CreateKeywordAsync(category, firstKeyword);
+        var secondKeywordResult = await keywordService.CreateKeywordAsync(category, secondKeyword);
+
+        // act
+        var editResult = await keywordService.EditKeywordAsync(category, firstKeyword, secondKeyword);
+
+        // assert
+        categoryResult.Should().Be(ResultType.Created);
+        firstKeywordResult.Should().Be(ResultType.Created);
+        secondKeywordResult.Should().Be(ResultType.Created);
+        editResult.Should().Be(ResultType.Duplicate);
+        await AssertCategoryExists(category);
+        await AssertKeywordExists(category, firstKeyword);
+        await AssertKeywordExists(category, secondKeyword);
+    }
 
     [TestMethod]
     public async Task CanGetKeywords()
