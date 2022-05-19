cc81bf53236f1571b8cd7428079346565364bb7b
Jason Racey <jasonracey@gmail.com>
2022-05-18 22:35:00 -0700

    Configure app insights

--- Keyworder/Program.cs
+++ Keyworder/Program.cs
@@ -1,4 +1,3 @@
-using BlazorApplicationInsights;
 using Keyworder;
 using Keyworder.Data;
 using Microsoft.ApplicationInsights.AspNetCore.Extensions;
@@ -7,23 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // For reading these values during local development see the link below:
 // https://medium.com/datadigest/user-secrets-in-asp-net-core-with-jetbrains-rider-26c381177391
-var applicationInsightsConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"];
 var applicationInsightsInstrumentationKey = builder.Configuration["APPLICATIONINSIGHTS_INSTRUMENTATION_KEY"];
 var storageAccountConnectionString = builder.Configuration["STORAGE_ACCOUNT_CONNECTION_STRING"];
 
-builder.Services.AddApplicationInsightsTelemetry(new ApplicationInsightsServiceOptions
-{
-    ConnectionString = applicationInsightsConnectionString
-});
 using var loggerFactory = LoggerFactory.Create(loggingBuilder => loggingBuilder
-    .SetMinimumLevel(LogLevel.Trace)
+    .SetMinimumLevel(LogLevel.Information)
     .AddConsole()
     .AddApplicationInsights(applicationInsightsInstrumentationKey));
 
-builder.Services.AddBlazorApplicationInsights(async applicationInsights =>
+builder.Services.AddApplicationInsightsTelemetry(new ApplicationInsightsServiceOptions
 {
-    await applicationInsights.SetInstrumentationKey(applicationInsightsInstrumentationKey);
-    await applicationInsights.TrackPageView();
+    InstrumentationKey = applicationInsightsInstrumentationKey,
+    EnableAdaptiveSampling = false
 });
 builder.Services.AddKeyworderServices(storageAccountConnectionString, loggerFactory.CreateLogger<KeywordService>());
 builder.Services.AddRazorPages();
